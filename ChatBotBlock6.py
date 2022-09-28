356dcd6d67698366d236dcea68253069e05b567b
xo1ox <114187225+xo1ox@users.noreply.github.com>
2022-09-28 09:42:51 +0200

    Add files via upload

--- ChatBotBlock6.py
+++ ChatBotBlock6.py
@@ -1,9 +1,6 @@
 #ChatBotBlock6
-# -*- coding: utf-8 -*-
 from csv import excel
-
-s = 'Support-Bot'
-
+import math
 
 #########################################################################################
 
@@ -12,114 +9,84 @@ def ChatBotStart():
    
     options = ['Word', 'Excel', 'PowerPoint', 'Outlook', 'Access', 'Visio']
     
-    print(s + ": Willkommen, Ich bin " + s + ", bitte nennen Sie mir ein Produkt von Microsoft wo Sie Probleme haben.")
-    print(s + ": Sollten Sie kein Problem haben, antworten Sie bitte mit: Nein")
+    print("Joe-Mama: Willkommen, Ich bin Joe-Mama, bitte nennen Sie mir ein Produkt von Microsoft, mit dem Sie Probleme haben.")
+    print("Joe-Mama: Sollten Sie kein Problem haben, antworten Sie bitte mit: Nein")
     while True:
-        global produkt
         produkt = input("")
-        
         if produkt in options:
-           WelchesProgramm()
-#########################################################################################
-
-#Auswahl des Programms
-def WelchesProgramm():
-
-    match produkt:
-
-        case 'Word':
-            WordAbfrage()
-
-        case 'Excel':
-            ExcelAbfrage()
-
-        case 'PowerPoint':
-            PowerPointAbfrage()
-
-        case 'Outlook':
-            OutlookAbfrage()
-
-        case'Access':
-            AccessAbfrage()
-
-        case 'Visio':
-            VisioAbfrage()
-
-#########################################################################################
+           break
+#####################################################################################
+#WORD-Abfrage
 
 def WordAbfrage():
     wordoptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit Word genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit Word genauer beschreiben?")
         
     while True:
         wordprodukt = input("")
         if wordprodukt in wordoptions:
-            print(s + ": Hier sind die Lösungen")
-            break
-    input("Press ENTER to exit")
-#########################################################################################
+            print("Joe-Mama: Hier sind die Lösungen")   
+#######################################################################################
+
+#Excel-Abfrage
 
 def ExcelAbfrage():
     exceloptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit Excel genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit Excel genauer beschreiben?")
 
     while True:
         excelprodukt = input("")
         if excelprodukt in exceloptions:
-            print(s + ": Hier sind die Lösungen")
-            break
-    input("Press ENTER to exit")
+            print("Joe-Mama: Hier sind die Lösungen")
 #########################################################################################
+#PowerPoint-Abfrage
 
 def PowerPointAbfrage():
     powerpointoptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit PowerPoint genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit PowerPoint genauer beschreiben?")
 
     while True:
         powerpointprodukt = input("")
         if powerpointprodukt in powerpointoptions:
-            print(s + ": Hier sind die Lösungen")
-            break
-    input("Press ENTER to exit")
+            print("Joe-Mama: Hier sind die Lösungen")
 #########################################################################################
+# Outlook Abfrage
 
 def OutlookAbfrage():
     outlookoptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit Outlook genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit Outlook genauer beschreiben?")
 
     while True:
         outlookprodukt = input("")
         if outlookprodukt in outlookoptions:
-            print(s + ": Hier sind die Lösungen")
-            break
-    input("Press ENTER to exit")
+            print("Joe-Mama: Hier sind die Lösungen")
 #########################################################################################
+#Access Abfrage
 
 def AccessAbfrage():
     accessoptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit Access genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit Access genauer beschreiben?")
 
     while True:
         accessprodukt = input("")
         if accessprodukt in accessoptions:
-            print(s + ": Hier sind die Lösungen")
-            break
-    input("Press ENTER to exit")
+            print("Joe-Mama: Hier sind die Lösungen")
 #########################################################################################
+#Visio-Abfrage
 
 def VisioAbfrage():
     visiooptions = ['Kaputt']
-    print(s + ": Können Sie mir Ihr Problem mit Visio genauer beschreiben?")
+    print("Joe-Mama: Können Sie mir Ihr Problem mit Visio genauer beschreiben?")
 
     while True:
         visioprodukt = input("")
         if visioprodukt in visiooptions:
-             print(s + ": Hier sind die Lösungen")
-             break
-    input("Press ENTER to exit")
+             print("Joe-Mama: Hier sind die Lösungen")
 #########################################################################################
 
 
-#Ausführen vom Programm
-
 ChatBotStart()
+
+
+
+input('Press ENTER to exit')
